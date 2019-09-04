e8c4f92c4e02404db63bb7ddb9a132e576d05dc3
John Morrison <jjosephmorrison@gmail.com>
2019-09-04 13:18:01 -0400

    pipfile

--- lambdata_jjmorr13/lambdata.py
+++ lambdata_jjmorr13/lambdata.py
@@ -1,5 +1,5 @@
 import pandas as pd 
-import numpy as np 
+import numpy as np
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
@@ -15,9 +15,10 @@ def train_val_test(df, target):
     The train set is 60% of the data, while validation and test set are 20% each
     Proper use of function = " train, val, test = train_val_test(df, 'target') "
     '''
+
     # Sklearn to split train and test 80/20, stratify on target
-    train, test = train_test_split(train, test_size=0.2, train_size=0.8, 
-                                    stratify=train[target], random_state=42)
+    train, test = train_test_split(df, test_size=0.2, train_size=0.8, 
+                                    stratify=df[target], random_state=42)
     
     # Split train and validation 60/20 from original size, stratify on target
     train, val = train_test_split(train, test_size=0.25, train_size=0.75,
--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/JohnMorrisonn/lambdata",
     packages=setuptools.find_packages(),
+    license='MIT'
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
